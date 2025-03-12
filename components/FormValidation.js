fe332e602145f7f1313567f58074be2249bb8eab
MartaPerezCoca <martaperezcoca@gmail.com>
2025-03-12 12:44:38 -0700

    add validator part 2

--- components/FormValidation.js
+++ components/FormValidation.js
@@ -9,48 +9,60 @@ class FormValidator {
     this._formEl = formEl;
   }
 
-// TODO - implement rest of the methods
-_showInputError() {}
+  _showInputError(inputElement, errorMessage) {
+    const errorElementId = `#${inputElement.id}-error`;
+    const errorElement = this._formEl.querySelector(errorElementId);
+    inputElement.classList.add(this._inputErrorClass);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(this._errorClass);
+  }
 
-_hideInputError() {}
+  _hideInputError(inputElement) {
+    const errorElementId = `#${inputElement.id}-error`;
+    const errorElement = this._formEl.querySelector(errorElementId);
+    inputElement.classList.remove(this._inputErrorClass);
+    errorElement.classList.remove(this._errorClass);
+    errorElement.textContent = "";
+  }
 
-_hasInvalidInput() {}
+  _checkInputValidity(inputElement) {
+    if (!inputElement.validity.valid) {
+      this._showInputError(inputElement, inputElement.validationMessage);
+    } else {
+      this._hideInputError(inputElement);
+    }
+  }
 
-_toggleButtonState() {}
+  _hasInvalidInput() {
+    return this._inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+    });
+  }
 
-
-
-
-
-  // implement this method checkInputValidity
-  //_checkInputValidity(inputElement) {
-   // if (!inputElement.validity.valid) {
-     // showInputError(
-       // this._formEl,
-       // this._inputElement,
-       // this._inputElement.validationMessage,
-       // settings
-     // );
-    //} else {
-     // hideInputError(this._formEl, this._inputElement, settings);
-    //}
+  _toggleButtonState() {
+    if (this._hasInvalidInput()) {
+      this._buttonElement.classList.add(this._inactiveButtonClass);
+      this._buttonElement.disabled = true;
+    } else {
+      this._buttonElement.classList.remove(this._inactiveButtonClass);
+      this._buttonElement.disabled = false;
+    }
   }
 
   _setEventListeners() {
     this._inputList = Array.from(
       this._formEl.querySelectorAll(this._inputSelector)
     );
-    // TODO - FINISH implementng event listeners
-    //const buttonElement = this._formEl.querySelector(
-    //settings.submitButtonSelector
-    //);
+    this._buttonElement = this._formEl.querySelector(
+      this._submitButtonSelector
+    );
 
-    //toggleButtonState(this._inputList, this._buttonElement, settings);
+    this._toggleButtonState();
 
     this._inputList.forEach((inputElement) => {
       inputElement.addEventListener("input", () => {
-        this._checkInputValidity(this._inputElement);
-        //this._toggleButtonState(this._inputList, this._buttonElement, settings);
+        this._checkInputValidity(inputElement);
+        this._toggleButtonState();
       });
     });
   }
