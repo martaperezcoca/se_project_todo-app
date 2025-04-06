512c903c19928575a3b8b251a737b21d932a0bec
MartaPerezCoca <martaperezcoca@gmail.com>
2025-04-06 14:10:25 -0700

    add first part creation section class

---
+++ components/Section.js
@@ -0,0 +1,13 @@
+class Section {
+  constructor({ items, renderer, containerSelector }) {
+    this._items = items;
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
+  }
+
+  renderItems() {}
+
+  addItem() {}
+}
+
+export default Section;
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,23 @@ import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 import { initialTodos, validationConfig } from "../utils/constants.js";
 import Todo from "../components/Todo.js";
 import FormValidator from "../components/FormValidation.js";
+import Section from "../components/Section.js";
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
 const addTodoForm = document.forms["add-todo-form"];
 const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
 const todosList = document.querySelector(".todos__list");
+
+const section = new Section({
+  items: [], // pass initial todos
+  renderer: () => {
+    //TODO - write the function
+  },
+  containerSelector: ".todos__list",
+});
+
+//call section instances renderItems method
 
 const openModal = (modal) => {
   modal.classList.add("popup_visible");
